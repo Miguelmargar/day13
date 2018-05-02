28ee1012098927146f91f9a96e0015842f6d97b2
Miguel Martinez <miguelitalia@hotmail.com>
2018-05-02 15:02:03 +0000

    star wars api retrieve and show of luke skywalker

--- api1.js
+++ api1.js
@@ -1,13 +1,30 @@
 //how to retrieve data from an API
-
 let request = new XMLHttpRequest();
 //XML stands for extensible mark up language - just cos it uses the above doesn't mean that it will be in xml format
-request.onreadystatechange() = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-}
 
-request.open("GET", https://swapi.com/api/people/1")
+let dataSource = document.getElementById("data");
+
+function displayNicelyl(apiData) {
+    let newData = JSON.parse(apiData);
+        let count = 0;
+        for (let i in newData) {
+            count++;
+            dataSource.innerHTML +="<strong>" + i + "</strong>" + ": " + newData[i] + "," + "<br>";
+            if (count == 7) {
+                break;
+            }
+        }
+    }
+
+
+//      dataSource.innerHTML = "<strong>Name:</strong> " + newData.name;
+
+request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        displayNicelyl(this.responseText);
+    }
+};
+
+request.open("GET", "https://swapi.co/api/people/1/");
 
 request.send();
